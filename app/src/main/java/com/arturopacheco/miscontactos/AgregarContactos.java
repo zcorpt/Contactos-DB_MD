3d0da4647c802fabde3ca7c7f05bed9c0b653f3a
zcorpt <huero4@hotmail.com>
2017-05-26 08:55:37 -0500

    Cambio de vistas

--- app/src/main/java/com/arturopacheco/miscontactos/AgregarContactos.java
+++ app/src/main/java/com/arturopacheco/miscontactos/AgregarContactos.java
@@ -1,7 +1,10 @@
 package com.arturopacheco.miscontactos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AgregarContactos extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AgregarContactos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_contactos);
+
+        Button agregar = (Button)findViewById(R.id.registrar);
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lista_contactos = new Intent(AgregarContactos.this, MainActivity.class);
+                startActivity(lista_contactos);
+            }
+        });
     }
 }
